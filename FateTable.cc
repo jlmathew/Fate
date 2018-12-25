3c2bee357e1db69b72f2dd19265febd7ad0b261d
James Mathewson <jlmathew@soe.ucsc.edu>
2018-12-25 14:22:22 -0800

    added fate table

---
+++ FateTable.cc
@@ -0,0 +1,21 @@
+#define "FateTable.h"
+
+class TableRouteInfo : public TableBase
+{
+	 
+	public:
+    TableRouteInfo();
+    virtual ~TableRouteInfo();k
+  
+    GetAllPhys(std::list< PhyName &phyName>);
+    GetAllDestinations(const PhyName &phyName, std::list< NodeName> &destination);
+    GetAllNeighbors(const PhyName &phyName, const NodeName &destination, NodeName &neighbor);
+    GetAllProperties(const PhyName &phyName, const NodeName &destination,const NodeName &neighbor,  const RouteInfo &updatedInfo);
+    UpdateTable(const PhyName &phyName, const NodeName &destination,const NodeName &neighbor,  const RouteInfo &updatedInfo);
+    UpdateTable(const PhyName &phyName, const NodeName &destination,const NodeName &neighbor,  const RouteInfo &updatedInfo, std::list<FullRouteCriteria> &uniqueRouteCharacteristics);
+    //GetEntries(const PhyName &phyName, const NodeName &neighbor, const NodeName &destination, std::list <RouteInfo> &updatedInfo);
+    GetMatches(const std::list<PhyName> &phyList, const std::list<NodeName> &destList, const std::list<NodeName> neighborList, const std::list< RouteCriteria> &routeCriteriaList, std::list<FullRouteInfo_t> &routeMatchList);
+
+
+};
+
