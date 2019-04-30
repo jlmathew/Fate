73f5dcf31aab2deb8eb1810b0482ffee6f335220
James Mathewson <jlmathew@soe.ucsc.edu>
2019-04-30 08:55:17 -0700

    update nomralizer

--- NormalizeValue.h
+++ NormalizeValue.h
@@ -10,7 +10,7 @@ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-The only restriction to usage is to fully credit the original work, in some manner, 
+The only restriction to usage is to fully credit the original work, in some manner,
 such as by project name, website, academic reference, documentation, and/or other
 non obscured means.
 Exceptions to this are allowed by prior permission from the originator (James Mathewson)
@@ -54,14 +54,16 @@ SOFTWARE.
 template<class T>
 class Normalize
 {
-  public:
+public:
     Normalize() { }
-    Normalize(ConfigWrapper &config) { Config(config); }
-    virtual ~Normalize() 
+    Normalize(ConfigWrapper &config) {
+        Config(config);
+    }
+    virtual ~Normalize()
     {
     }
     virtual void Print(std::ostream &os) const {
-    os << "_N_";
+        os << "_N_";
     }
     virtual void Config(ConfigWrapper &config) {}
     virtual void InsertValue(T a)
@@ -70,65 +72,69 @@ class Normalize
 
     virtual bool DeleteValue(T a)
     {
-	    return true;
+        return true;
     }
-   
-    virtual 
-	    double EvaluateValue(T) {
-	    return 0.0;
+
+    virtual
+    double EvaluateValue(T) {
+        return 0.0;
     }
-  static const dataNameType_t & IdName (void)
-  {
-    static const dataNameType_t idName ("Normalized");
-      return idName;
-  }
+    static const dataNameType_t & IdName (void)
+    {
+        static const dataNameType_t idName ("Normalized");
+        return idName;
+    }
 
 };
 
 template<class T>
 class NormalizeValue : public Normalize<T>
 {
-  public:
+public:
     NormalizeValue() {}
-    NormalizeValue(ConfigWrapper &xml) { Config(xml);}
-    virtual ~NormalizeValue() 
-    { m_values.clear();}
-    
+    NormalizeValue(ConfigWrapper &xml) {
+        Config(xml);
+    }
+    virtual ~NormalizeValue()
+    {
+        m_values.clear();
+    }
+
     void Config(ConfigWrapper &config) {
-       Normalize<T>::Config(config);
+        Normalize<T>::Config(config);
     }
     void Print(std::ostream &os) const {
-    os << "_Nv_";
+        os << "_Nv_";
     }
     void InsertValue(T a)
     {
-       m_values.insert(a);
+        m_values.insert(a);
     }
     bool DeleteValue(T a)
     {
-typename std::multiset<T>::iterator it;
-	it = m_values.find(a);
-	bool ret = true;
-	if (it != m_values.end())
+        typename std::multiset<T>::iterator it;
+        it = m_values.find(a);
+        bool ret = true;
+        if (it != m_values.end())
         {
-           m_values.erase(it);
+            m_values.erase(it);
         } else {
             ret=false;
         }
-	return ret;
+        return ret;
     }
-   
-    double EvaluateValue(T) {
- throw std::invalid_argument( "baseclass called, should be a derived class" );
-	    return 0.0;
-    }
-      static const dataNameType_t & IdName (void)
-  {
-    static const dataNameType_t idName ("NormalizeValue");
-      return idName;
-  }
 
-  protected:
+    double EvaluateValue(T) {
+        throw std::invalid_argument( "baseclass called, should be a derived class" );
+        return 0.0;
+    }
+    static const dataNameType_t & IdName (void)
+    {
+        static const dataNameType_t idName ("NormalizeValue");
+        return idName;
+    }
+
+protected:
     std::multiset<T> m_values;
     T GetMinimum();
     T GetMaximum();
@@ -139,22 +145,24 @@ typename std::multiset<T>::iterator it;
 template <class T>
 class NormalMatch : public Normalize<T>
 {
-  public: 
+public:
     NormalMatch() {}
     ~NormalMatch() {}
-    NormalMatch(ConfigWrapper &config) { Config(config); }
-    
+    NormalMatch(ConfigWrapper &config) {
+        Config(config);
+    }
+
     void Config(ConfigWrapper &config) {
-       Normalize<T>::Config(config);
-     double m_minValue = 0.0;
-     double m_maxValue = 0.0;
-      m_minValue = config.GetAttribute("matchLow", m_minValue); 
-      m_maxValue = config.GetAttribute("matchHigh", m_maxValue); 
-      RangeData<T> confMatch(static_cast<T>(m_minValue),static_cast<T>(m_maxValue));
-      InsertMatchValue(confMatch);
+        Normalize<T>::Config(config);
+        double m_minValue = 0.0;
+        double m_maxValue = 0.0;
+        m_minValue = config.GetAttribute("matchLow", m_minValue);
+        m_maxValue = config.GetAttribute("matchHigh", m_maxValue);
+        RangeData<T> confMatch(static_cast<T>(m_minValue),static_cast<T>(m_maxValue));
+        InsertMatchValue(confMatch);
     }
     void Print(std::ostream &os) const {
-    os << "_Nm_"; 
+        os << "_Nm_";
     }
 
     void
@@ -164,53 +172,59 @@ class NormalMatch : public Normalize<T>
     }
     double EvaluateValue(T val)
     {
-       if (m_data.IsInRange(val)) {
-           return 1.0;
-       } else 
-	   return 0.0;
+        if (m_data.IsInRange(val)) {
+            return 1.0;
+        } else
+            return 0.0;
     }
-  static const dataNameType_t & IdName (void)
-  {
-    static const dataNameType_t idName ("NormalMatch");
-      return idName;
-  }
+    static const dataNameType_t & IdName (void)
+    {
+        static const dataNameType_t idName ("NormalMatch");
+        return idName;
+    }
 
-  private:
+private:
     RangeData<T> m_data;
 };
 
 template <class T>
 class GeometricRanked : public Normalize<T>
 {
-	public:
+public:
     GeometricRanked(): m_invert(false) {};
-    GeometricRanked(ConfigWrapper &config): m_invert(false) { Config(config);};
+    GeometricRanked(ConfigWrapper &config): m_invert(false) {
+        Config(config);
+    };
     ~GeometricRanked() {};
-    void SetInvert(bool inv) { m_invert = inv;}
+    void SetInvert(bool inv) {
+        m_invert = inv;
+    }
     void Config(ConfigWrapper &config) {
-       Normalize<T>::Config(config);
-       m_invert = config.GetAttributeBool ("invertValue", m_invert);
+        Normalize<T>::Config(config);
+        m_invert = config.GetAttributeBool ("invertValue", m_invert);
     }
     void Print(std::ostream &os) const {
-    os << "_Ngr:" << m_invert << "_";
+        os << "_Ngr:" << m_invert << "_";
     }
 
     double EvaluateValue(T val)
     {
-       double ret = 1.0;
-       if (val != 0.0) {ret = 1.0/val;}
-       if (m_invert) {
-          ret = 1.0-ret;
-       } 
-       return ret;
+        double ret = 1.0;
+        if (val != 0.0) {
+            ret = 1.0/val;
+        }
+        if (m_invert) {
+            ret = 1.0-ret;
+        }
+        return ret;
     }
-      static const dataNameType_t & IdName (void)
-  {
-    static const dataNameType_t idName ("GeometricMatch");
-      return idName;
-  }
+    static const dataNameType_t & IdName (void)
+    {
+        static const dataNameType_t idName ("GeometricMatch");
+        return idName;
+    }
 
-	private:
+private:
     bool m_invert;
 };
 
@@ -218,81 +232,93 @@ class GeometricRanked : public Normalize<T>
 template <class T>
 class NormalRanked : public NormalizeValue<T>
 {
-	public:
+public:
     enum normalRankedValueOption_t : uint16_t
-    {  
-	inValid,
-	floor,
-	ceiling,
-	fullRange,
-	invfloor,
-	invceiling,
-	invfullRange
+    {
+        inValid,
+        floor,
+        ceiling,
+        fullRange,
+        invfloor,
+        invceiling,
+        invfullRange
     };
     NormalRanked(): m_strVal("ceiling"), m_valOption(ceiling) {}
-    NormalRanked(ConfigWrapper &config):m_strVal("ceiling"), m_valOption(ceiling)  { 
-	    Config(config);
+    NormalRanked(ConfigWrapper &config):m_strVal("ceiling"), m_valOption(ceiling)  {
+        Config(config);
     }
     virtual ~NormalRanked() { }
     void Config(ConfigWrapper &config) {
-       NormalizeValue<T>::Config(config);
-       m_strVal = config.GetAttribute("value_type", m_strVal);
+        NormalizeValue<T>::Config(config);
+        m_strVal = config.GetAttribute("value_type", m_strVal);
 
-       if (m_strVal == "ceiling") { m_valOption = ceiling;}
-       else if (m_strVal == "floor") { m_valOption = floor;}
-       else if (m_strVal == "fullRange") { m_valOption = fullRange;}
-       else if (m_strVal == "invceiling") { m_valOption = invceiling;}
-       else if (m_strVal == "invfloor") { m_valOption = invfloor;}
-       else if (m_strVal == "invfullRange") { m_valOption = invfullRange;}
+        if (m_strVal == "ceiling") {
+            m_valOption = ceiling;
+        }
+        else if (m_strVal == "floor") {
+            m_valOption = floor;
+        }
+        else if (m_strVal == "fullRange") {
+            m_valOption = fullRange;
+        }
+        else if (m_strVal == "invceiling") {
+            m_valOption = invceiling;
+        }
+        else if (m_strVal == "invfloor") {
+            m_valOption = invfloor;
+        }
+        else if (m_strVal == "invfullRange") {
+            m_valOption = invfullRange;
+        }
 
     }
     void Print(std::ostream &os) const {
-    os << "_Nr:" << m_valOption << "_";
+        os << "_Nr:" << m_valOption << "_";
     }
-    double EvaluateValue(T val) 
+    double EvaluateValue(T val)
     {
-	    //should we check if the provided value exists?
-	    //if not exist, should we throw an exception
-	    //TODO add flag to check if values exists, and if not, throw exception
-	if (this->m_values.empty())
-		return 0.0;
+        //should we check if the provided value exists?
+        //if not exist, should we throw an exception
+        //TODO add flag to check if values exists, and if not, throw exception
+        if (this->m_values.empty())
+            return 0.0;
         T min = *(this->m_values).cbegin(); //need something better?
-	T max = *(this->m_values).crbegin();
-	if (min == max)
-		return 1.0;
-	double typeval=1.0;
-	switch (m_valOption) {
-           case floor:  //can be 0.0
-		typeval = (double) (static_cast<double>(val-min))/(max-min);
-		break;
-           case ceiling:  // can NOT be 0.0
-		typeval = (double) (static_cast<double>(val-min)+1.0)/(max-min+1.0);
-		break;
-           case fullRange:  // can NOT be 0.0
-		typeval = (double) (val)/(max);
-		break;
-           case invfloor:  
-		typeval = 1.0 - (double) (static_cast<double>(val-min))/(max-min);
-		break;
-           case invceiling:  
-		typeval = 1.0 - (double) (static_cast<double>(val-min)+1.0)/(max-min+1.0);
-		break;
-           case invfullRange:  
-		typeval = 1.0 - (double) (val)/(max);
-		break;
-           default:
-		throw ("Unknown value option type");
-	}
-      return typeval;
+        T max = *(this->m_values).crbegin();
+        if (min == max)
+            return 1.0;
+        double typeval=1.0;
+        switch (m_valOption) {
+        case floor:  //can be 0.0
+            typeval = (double) (static_cast<double>(val-min))/(max-min);
+            break;
+        case ceiling:  // can NOT be 0.0
+            typeval = (double) (static_cast<double>(val-min)+1.0)/(max-min+1.0);
+            break;
+        case fullRange:  // can NOT be 0.0
+            typeval = (double) (val)/(max);
+            break;
+        case invfloor:
+            typeval = 1.0 - (double) (static_cast<double>(val-min))/(max-min);
+            break;
+        case invceiling:
+            typeval = 1.0 - (double) (static_cast<double>(val-min)+1.0)/(max-min+1.0);
+            break;
+        case invfullRange:
+            typeval = 1.0 - (double) (val)/(max);
+            break;
+        default:
+            throw ("Unknown value option type");
+        }
+        return typeval;
     }
-    
-      static const dataNameType_t & IdName (void)
-  {
-    static const dataNameType_t idName ("NormalRanked");
-      return idName;
-  }
 
-	private:
+    static const dataNameType_t & IdName (void)
+    {
+        static const dataNameType_t idName ("NormalRanked");
+        return idName;
+    }
+
+private:
     std::string m_strVal;
     normalRankedValueOption_t m_valOption;
 };
@@ -302,60 +328,62 @@ class NormalRanked : public NormalizeValue<T>
 template <class T>
 class StepRanked: public NormalizeValue<T>
 {
-	public:
-   StepRanked() : m_rankHigh(true) {}
-    StepRanked(ConfigWrapper &config) : m_rankHigh(true) { Config(config);}
-   ~StepRanked() { } 
+public:
+    StepRanked() : m_rankHigh(true) {}
+    StepRanked(ConfigWrapper &config) : m_rankHigh(true) {
+        Config(config);
+    }
+    ~StepRanked() { }
     void Config(ConfigWrapper &config) {
-       NormalizeValue<T>::Config(config);
-       m_rankHigh = config.GetAttributeBool("rank_high", m_rankHigh);
+        NormalizeValue<T>::Config(config);
+        m_rankHigh = config.GetAttributeBool("rank_high", m_rankHigh);
 
 
     }
-  static const dataNameType_t & IdName (void)
-  {
-    static const dataNameType_t idName ("StepRanked");
-      return idName;
-  }
-    void Print(std::ostream &os) const {
-    os << "_Nsr_";
-    }
-
-    double EvaluateValue(T val) 
+    static const dataNameType_t & IdName (void)
     {
-	uint32_t totalSize = this->m_values.size();
-	if (0 == totalSize)
-		return 0.0;
-	
-	if (1 == totalSize)
-		return 1.0;
+        static const dataNameType_t idName ("StepRanked");
+        return idName;
+    }
+    void Print(std::ostream &os) const {
+        os << "_Nsr_";
+    }
+
+    double EvaluateValue(T val)
+    {
+        uint32_t totalSize = this->m_values.size();
+        if (0 == totalSize)
+            return 0.0;
+
+        if (1 == totalSize)
+            return 1.0;
         if (m_rankHigh) {
-           auto count = totalSize;
-	   for (auto i=this->m_values.crbegin(); i != this->m_values.crend(); i++) {
-              if (val == *i) {
-                  return (double) count/totalSize;
-	      } else {
-                  --count;
-	      }
-	   }
-	      throw ("Value not found");
-	} else {
-           auto count = 1;
-	   for (auto i=this->m_values.cbegin(); i != this->m_values.cend(); i++) {
-              if (val == *i) {
-                  return (double) count/totalSize;
-	      } else {
-                  ++count;
-	      }
-	   }
-	      throw ("Value not found");
-	}	
+            auto count = totalSize;
+            for (auto i=this->m_values.crbegin(); i != this->m_values.crend(); i++) {
+                if (val == *i) {
+                    return (double) count/totalSize;
+                } else {
+                    --count;
+                }
+            }
+            throw ("Value not found");
+        } else {
+            auto count = 1;
+            for (auto i=this->m_values.cbegin(); i != this->m_values.cend(); i++) {
+                if (val == *i) {
+                    return (double) count/totalSize;
+                } else {
+                    ++count;
+                }
+            }
+            throw ("Value not found");
+        }
 
     }
-    private:
+private:
     bool m_rankHigh;
 
-    
+
 };
 //class StepRanked (even parsing)
 #endif
